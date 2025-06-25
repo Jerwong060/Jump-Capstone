8b6a74634796112135a08677d2bbc84a70a31f8a
Jerrick Wong <108111550+Drogonic@users.noreply.github.com>
2025-06-24 20:30:47 -0400

    Added APP() to main

--- src/main/java/com/jump/capstone/App.java
+++ src/main/java/com/jump/capstone/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new App();
+    }
+
+    public App(){
+        System.out.println("Hi");
     }
 }
