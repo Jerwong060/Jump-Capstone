b7dc14e2df29bd7775cca2527437a807130b8de5
Jerrick Wong <jerwong060@gmail.com>
2025-07-06 19:03:04 -0400

    Added comment to connection manager

--- src/main/java/com/jump/capstone/connection/ConnectionManager.java
+++ src/main/java/com/jump/capstone/connection/ConnectionManager.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 
 public class ConnectionManager {
-
-    private static final String URL = "jdbc:mysql://localhost:3306/music_track";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "123456789";
+	//ADD IN DATABASE INFOMATION TO THIS
+    private static final String URL = "";
+	private static final String USERNAME = "";
+	private static final String PASSWORD = "";
 
     private static Connection connection = null;
 
